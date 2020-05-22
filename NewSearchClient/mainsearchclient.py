847d833dc06fa63c36b7f32138514b02f4a3426a
s192770 <s192770@student.dtu.dk>
2020-05-22 09:13:35 +0200

    Comment csv function

--- NewSearchClient/mainsearchclient.py
+++ NewSearchClient/mainsearchclient.py
@@ -1,5 +1,5 @@
 import argparse
-import pandas as pd
+#import pandas as pd
 import sys
 
 import configuration
@@ -27,17 +27,17 @@ def main(strategy_str: 'str', df: 'object'):
     
     # Solve the level (resolve the plan and get a path from initial state to goal state).
     commands = planner.resolve()
-    for command in commands:  
-        print(str(command.jointaction), file=sys.stderr, flush=True)
+    '''for command in commands:  
+        print(str(command.jointaction), file=sys.stderr, flush=True)'''
         
     # Write results in CSV file if needed
-    if df is not None:
+    '''if df is not None:
         df = df.append({'level': com.level_name, 'strategy': strategy_str, 'num_agents': len(planner.start_state.agents),
                    'num_boxes': len(planner.start_state.boxes), 'num_colors': len(set(planner.start_state.colors.values())),
                    'solved': 1,'num_steps': len(commands), 'runtime': planner.strategy.time_spent(),
                    'explored': planner.strategy.explored_count(), 'frontier': planner.strategy.frontier_count(),
                    'generated': planner.strategy.explored_count() + planner.strategy.frontier_count(),
-                   'mem': configuration.get_memory_usage()}, ignore_index=True)
+                   'mem': configuration.get_memory_usage()}, ignore_index=True)'''
     
     # Send actions to server.
     com.send_commands_to_server(commands)
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple client based on state-space graph search.')
     parser.add_argument('--max-memory', metavar='<MB>', type=float, default=2048.0, help='The maximum memory usage allowed in MB (soft limit, default 2048).')
     
-    parser.add_argument('--csv', type=str, help='The absolute path to the CSV file where write the results.')
+    '''parser.add_argument('--csv', type=str, help='The absolute path to the CSV file where write the results.')'''
     
     # Parsing strategy args.
     strategy_group = parser.add_mutually_exclusive_group()
@@ -68,18 +68,18 @@ if __name__ == '__main__':
     configuration.max_memory_usage = args.max_memory
     
     # Prepare CSV file to print results
-    if args.csv:
+    '''if args.csv:
         try:
             df = pd.read_csv(args.csv)
         except:
             df = pd.DataFrame(columns = ['level','strategy','num_agents','num_boxes','num_colors','solved','num_steps','runtime','explored','frontier','generated','mem'])
-    else:
-        df = None
+    else:'''
+    df = None
     
     # Run client.
     df = main(args.strategy, df)
     
     # Save CSV file
-    if df is not None:
-        df.to_csv(args.csv, index=False)
+    '''if df is not None:
+        df.to_csv(args.csv, index=False)'''
     
